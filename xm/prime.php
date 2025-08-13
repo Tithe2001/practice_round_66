b97ef01c161b382e3deb3e30b1c7827b974eee99
Suprova Shemanto <suprovashemanto@gmail.com>
2025-08-13 13:04:34 +0600

    pf

---
+++ xm/prime.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>prime number</title>
+</head>
+<body>
+    
+
+<h1>Find wheather the number is prime or not.</h1> 
+
+<form action="prime.php" method="GET">
+
+    <input type="text" name="number" placeholder="give the number..."><br><br>
+    <input type="submit" name="Submit" >
+
+</form>
+
+
+<?php
+
+if(isset($_GET["Submit"]))
+    $n= $_GET["number"];
+
+
+if(){
+
+
+    
+}
+
+
+?>
+
+
+</body>
+</html>
